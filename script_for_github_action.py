6801dea8319f9ecb9646ebd6bcab30fa2f4ff6a1
Laney McGlohon <stitching.coder@gmail.com>
2025-05-06 11:56:59 -0700

    parse yaml file

---
+++ script_for_github_action.py
@@ -0,0 +1,35 @@
+import yaml
+
+def parse_yaml(file_path):
+    """
+    Parses a YAML file and returns the content as a dictionary.
+
+    :param file_path: Path to the YAML file
+    :return: Dictionary containing the parsed YAML content
+    """
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+def get_nested_keys(file_path):
+    """
+    Retrieves nested keys from a YAML file.
+
+    :param file_path: Path to the YAML file
+    :return: List of nested keys
+    """
+    data = parse_yaml(file_path)
+    nested_keys = []
+
+    def extract_keys(d, parent_key=''):
+        for k, v in d.items():
+            new_key = f"{parent_key}.{k}" if parent_key else k
+            if isinstance(v, dict):
+                extract_keys(v, new_key)
+            else:
+                nested_keys.append(new_key)
+
+    extract_keys(data)
+    return nested_keys
+
+nest_keys = get_nested_keys(file_path="generic.yaml")
+print(nest_keys)
